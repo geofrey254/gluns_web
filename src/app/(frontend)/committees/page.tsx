45e74b74e5d53384f08a731ae9afbf52deb62612
geofrey254 <devongeoffreymaina@gmail.com>
2026-02-26 20:51:52 +0300

    Added committees

--- src/app/(frontend)/committees/page.tsx
+++ src/app/(frontend)/committees/page.tsx
@@ -1,3 +1,5 @@
+export const dynamic = 'force-dynamic'
+
 import React from 'react'
 import CommitteeHero from '@/components/committeepage/CommitteeHero'
 import CommitteeList from '@/components/committeepage/CommitteeList'
--- src/components/committeepage/CommitteeList.tsx
+++ src/components/committeepage/CommitteeList.tsx
@@ -18,7 +18,7 @@ export default async function CommitteeList() {
 
       {/* Cards Grid */}
       <div className="max-w-7xl 2xl:max-w-full mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
-        {committee.map((committee, index) => (
+        {committee.reverse().map((committee, index) => (
           <div
             key={committee.id}
             className="group relative bg-white rounded-3xl shadow-lg hover:shadow-2xl transition-all duration-500 overflow-hidden border border-gray-100"

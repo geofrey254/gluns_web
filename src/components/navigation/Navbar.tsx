893c4d4a1157656d2b4b85d8440ff2a44d9c0d7c
geofrey254 <devongeoffreymaina@gmail.com>
2026-03-12 18:57:27 +0300

    Removed academy url temporarily

--- src/components/navigation/Navbar.tsx
+++ src/components/navigation/Navbar.tsx
@@ -34,7 +34,7 @@ export default function Navbar() {
     { name: 'The Secretariat', link: '/the-secretariat' },
     { name: 'Events', link: '/events' },
     { name: 'Committees', link: '/committees' },
-    { name: 'Academy', link: '/academy' },
+    // { name: 'Academy', link: '/academy' },
     { name: 'Blog', link: '/blog' },
     { name: 'Contact', link: '/contact' },
   ]

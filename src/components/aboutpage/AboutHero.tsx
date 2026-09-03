f9bde9a9684afce140fd2bbdadef0ae81363fdfc
geofrey254 <devongeoffreymaina@gmail.com>
2026-09-03 09:38:47 +0300

    About hero edit

--- src/components/aboutpage/AboutHero.tsx
+++ src/components/aboutpage/AboutHero.tsx
@@ -1,70 +1,19 @@
 'use client'
 import React from 'react'
-import { motion } from 'framer-motion'
-import Image from 'next/image'
 
 export default function AboutHero() {
-  const containerVariants = {
-    hidden: { opacity: 0 },
-    visible: {
-      opacity: 1,
-      transition: { staggerChildren: 0.2, duration: 0.8 },
-    },
-  }
-
-  const fadeUp = {
-    hidden: { opacity: 0, y: 40 },
-    visible: { opacity: 1, y: 0, transition: { duration: 0.8, ease: 'easeOut' as const } },
-  }
-
   return (
-    <motion.section
-      id="about-hero"
-      variants={containerVariants}
-      initial="hidden"
-      whileInView="visible"
-      viewport={{ once: true, amount: 0.3 }}
-      className="py-20 md:py-16 px-8 w-full md:px-12 2xl:px-16 bg-[#104179] dark:border-t dark:border-white relative z-30 shadow-2xl overflow-hidden flex flex-col items-center justify-center gap-12 md:gap-8"
-    >
-      <div className="w-full h-full flex flex-col md:flex-row justify-center items-center">
-        {/* left */}
-        <motion.div
-          variants={fadeUp}
-          className="w-full flex flex-col order-2 mx-auto justify-center items-start md:order-2 md:w-[60%]"
-        >
-          <div className="inline-flex items-center gap-2 bg-white/10 backdrop-blur-sm border border-white/20 rounded-full px-5 py-2 mb-4">
-            <div className="w-2 h-2 bg-[#85c226] rounded-full animate-pulse"></div>
-            <span className="text-white text-xs font-semibold tracking-wider uppercase">
-              About GLUNS
-            </span>
-          </div>
+    <section className="relative bg-[#051220] min-h-[70vh] md:min-h-[50vh] lg:min-h-[50vh] flex px-6 md:px-8 2xl:px-16 items-center z-20 overflow-hidden">
+      {/* gradient overlay */}
+      <div className="absolute inset-0 bg-[url(/images/abouthero.jpg)] bg-cover bg-top" />
 
-          <div className="flex flex-col gap-3">
-            <h1 className="text-5xl lg:text-7xl font-semibold text-white leading-tight">
-              Empowering Future <span className='text-[#85c226]'>Global</span> Leaders
-            </h1>
+      <div className="absolute inset-0 bg-linear-to-tr from-[#051220]/90 via-[#051220]/75 to-[#051220]/45" />
 
-            <p className="text-white leading-relaxed text-xl md:text-2xl">
-              The Global Leaders United Nations Symposium (GLUNS) is a transformative leadership and
-              diplomacy platform designed for high school students who are ready to engage with the
-              world{"'"}s most pressing issues. Through carefully curated simulations, debates,
-              workshops, and cultural exchanges, GLUNS empowers young people to become confident
-              communicators, ethical leaders, and globally conscious citizens.
-            </p>
-          </div>
-        </motion.div>
-
-        {/* right */}
-        <motion.div
-          variants={fadeUp}
-          className="flex mb-12 md:mb-0 order-1 md:order-2 relative z-10 bg-white/10 backdrop-blur-md border border-white/20 rounded-3xl p-8 md:p-12 shadow-2xl hover:scale-105 transition-transform duration-500"
-        >
-          <Image src="/logos/4.png" width={500} height={500} alt="GLUNS Logo" className="w-72" />
-        </motion.div>
+      <div className="relative z-10 w-full pt-32 pb-20 justify-center items-center flex flex-col gap-4 md:gap-0 2xl:gap-0">
+        <h1 className="text-5xl md:text-6xl lg:text-7xl 2xl:text-8xl font-bold text-white text-center z-10 lg:max-w-2xl 2xl:max-w-4xl">
+          EMPOWERING FUTURE GLOBAL LEADERS{' '}
+        </h1>
       </div>
-
-      <div className="absolute top-20 -left-20 w-60 h-60 bg-white/10 rounded-full blur-3xl pointer-events-none"></div>
-      <div className="absolute bottom-20 -right-20 w-60 h-60 bg-white/1 rounded-full blur-3xl pointer-events-none"></div>
-    </motion.section>
+    </section>
   )
 }
